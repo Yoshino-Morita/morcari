d0e188da7a506683cf08cb96e8cde2e90ae1cb88
Yoshino-Morita <yoshino@yoshinonoMacPro.local>
2017-08-21 22:33:52 +0900

    購入機能の作成
    # what
    コントローラの編集
    カラムの追加
    
    # why
    購入機能実装のため

--- app/controllers/buy_controller.rb
+++ app/controllers/buy_controller.rb
@@ -6,8 +6,10 @@ class BuyController < ApplicationController
   end
 
   def buy
-   @user = current_user
-   @items = current_user.items.order("created_at DESC")
+    @user = current_user
+    @item = Item.find(params[:item_id])
+    @item.buyer_id = current_user.id
+    @item.status = "取引完了"
   end
 
   def buylist
